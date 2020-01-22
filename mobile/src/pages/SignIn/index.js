802e718c0e9c0fed3a1b4602e4bff0b596d7b30e
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2020-01-22 18:47:03 -0300

    feat: Criando os componente button e input

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {Text} from 'react-native';
 
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SignIn() {
     return (
         <Background>
             <Text>SignIn</Text>
+
+            <Input style={{marinTop: 30}} icon="call" placeholder="Tell me!" />
+            <Button>Entrar</Button>
         </Background>
     );
 }
