5c1c67ae1cf504f8f611478b355b415bfc185846
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2020-01-23 15:06:08 -0300

    feat: Configurando o redux

--- mobile/src/config/ReactotronConfig.js
+++ mobile/src/config/ReactotronConfig.js
@@ -1,9 +1,11 @@
 import Reactotron from 'reactotron-react-native';
 import {reactotronRedux} from 'reactotron-redux';
 import sagaPlugin from 'reactotron-redux-saga';
+import AsyncStorage from '@react-native-community/async-storage';
 
 if (__DEV__) {
-    const tron = Reactotron.configure({host: '192.168.100.16'})
+    const tron = Reactotron.setAsyncStorageHandler(AsyncStorage)
+        .configure({host: '192.168.100.16'})
         .useReactNative()
         .use(reactotronRedux())
         .use(sagaPlugin())
