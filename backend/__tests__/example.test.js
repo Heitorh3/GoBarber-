02bc4ea2cd617b1072938e51f27fdb06a30bd8e6
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2020-01-24 18:10:13 -0300

    feat: configurando o jest
    Configurando o ambiente de teste no backend da aplicaçao GoBarber

---
+++ backend/__tests__/example.test.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-undef */
+function soma(a, b) {
+    return a + b;
+}
+
+test('If i call soma function 4 and 5 it shold return 9', () => {
+    const result = soma(4, 5);
+
+    expect(result).toBe(9);
+});
