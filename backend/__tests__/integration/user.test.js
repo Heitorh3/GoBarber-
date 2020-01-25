513b97472cf4738f2b66e65f319e18efe58444e8
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2020-01-25 13:09:22 -0300

    feat: teste de retorno de um usuario recrm criado
    Testa se um usuario e retornado apos ser criado

--- backend/__tests__/integration/user.test.js
+++ backend/__tests__/integration/user.test.js
@@ -14,4 +14,19 @@ describe('User', () => {
 
         expect(response.body).toHaveProperty('id');
     });
+
+    test('Can get a user by name and e-mail', async () => {
+        const userPayload = {
+            name: 'Maria do Carmo',
+            email: 'maria@gmail.com',
+            password: '123456',
+        };
+
+        const response = await request(app)
+            .post('/users')
+            .send(userPayload);
+
+        expect(response.body.name).toEqual(userPayload.name);
+        expect(response.body.email).toEqual(userPayload.email);
+    });
 });
